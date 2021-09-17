26aca823dcf7c06b7874bb1ed89ae19a3bb55fbf
uuu0614 <33905956+uuu0614@users.noreply.github.com>
2021-09-17 14:30:01 +0900

    fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ OUTPUT_FILE_PREFIX = "./output"
 def main():
     with open(INPUT_FILE_PATH, "r") as f:
         text = f.read()
-    for depth in range(1,5):
+    for depth in range(0,5):
         summary = summarize(text, depth=depth)
         with open(OUTPUT_FILE_PREFIX + str(depth) + ".txt", "w") as f:
             f.write(summary)
